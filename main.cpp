0526136e7a3990cf100ae1959750444c9ee8f74b
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-12-12 12:11:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+using namespace std;
+
+float MAX (int d, float valori[]){
+    int a;
+    for (int j; j<d;j++){
+       if (valori[j]>a){
+           a=valori[j];
+       }
+    }
+    return a;
+}
+
+int main() {
+    int d;
+    float k;
+    cin>>d;
+    float valori[d];
+    for (int i=0;i<d;i++){
+        cin>>k;
+        valori[i]=k;
+    }
+    cout << MAX (d, valori);
+    return 0;
 }
